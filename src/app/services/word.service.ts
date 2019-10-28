1b815e24561238c1bd558d6c8d683fae62605ae4
NGOC SON <nsta@groupeastek.fr>
2019-10-28 11:40:06 +0100

    test edit

--- src/app/services/word.service.ts
+++ src/app/services/word.service.ts
@@ -5,6 +5,10 @@ import { Word } from '../data/word';
   providedIn: 'root'
 })
 export class WordService {
+  getCurrentWordList(): any {
+    throw new Error("Method not implemented.");
+  }
+  
   saveNewWord(newWord: Word) {
     let htmlContent = newWord.word + "\n" + newWord.wordDef;
     return { content: htmlContent };
